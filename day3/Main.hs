2cb6d760d55c160aacb40de2c22e2dd981b74e94
Kevin Shi <Precambria@gmail.com>
2025-12-03 22:33:20 -0800

    remove part1 logic

--- day3/Main.hs
+++ day3/Main.hs
@@ -1,21 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.List (nubBy, sortBy)
+import Data.List (sortBy)
 import Data.Function (on)
-import qualified Data.Set as Set
 import Debug.Trace
-
-uniqueByFirst :: [(Char, Int)] -> [(Char, Int)]
-uniqueByFirst = nubBy ((==) `on` fst)
-
-valuesAt :: (Int, Int) -> String -> Int
-valuesAt (x, y) s = read [s !! x, s !! y]
-
-allPairs :: [Int] -> [(Int, Int)]
-allPairs xs = [(x, y) | x <- xs, y <- xs, x < y]
-
-allJolts :: [String] -> [(Int, Int)] -> [Set.Set Int]
-allJolts banks comb = [Set.fromList $ map (`valuesAt` x) comb | x <- banks]
 
 maxV :: String -> Int -> String
 maxV st runway
@@ -33,8 +20,6 @@ maxV st runway
     show $ maximum $ map (read :: String -> Int) mr
       where
 
-        {-- prune results that don't make sense --}
-        {-- prune penalties: why pay higher price for the same value?? --}
         {-- Crucial insight is that you must choose the highest in that window --}
         penalties = take 1 $ sortBy (flip compare `on` fst) $
           take (length st - runway + 1) $ zip st [1..length st]
@@ -43,16 +28,10 @@ maxV st runway
 
 main :: IO()
 main = do
-  banks  <- fmap lines (readFile "input.txt")
-
-  {-- All combinations of switches --} 
-  let allComb = allPairs [0..length (head banks) - 1]   
-
-  {-- Results of banks x combinations --}
-  let result = allJolts banks allComb 
+  banks  <- fmap lines (readFile "example.txt")
 
   {-- Part 1 --}
-  print $ sum $ map maximum result
+  print $ sum $ map ((read :: (String -> Int)) . take 2 . flip maxV 2) banks
 
   {-- Part 2 --}
   print $ sum $ map ((read :: (String -> Int)) . take 12 . flip maxV 12) banks
